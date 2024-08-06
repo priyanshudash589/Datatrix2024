65409808abd19fb5be2ee2dadc83537736f01190
Priyanshu Dash <pdash3774@gmail.com>
2024-08-07 02:13:46 +0530

    full page is responsive

--- src/components/Speakers.jsx
+++ src/components/Speakers.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
+import React from "react";
 import Marquee from "react-fast-marquee";
-import { ProfileCard } from '../constants/ProfileCard';
+import { ProfileCard } from "../constants/ProfileCard";
 
 const Speakers = () => {
   return (
     <>
-    <div className='w-screen h-[45rem] bg-black text-center flex flex-col gap-2 p-7 pt-11'>
-        <span className='text-white text-4xl font-orbitron'>Data Innovations Conference Experts</span>
-        <span className='text-center font-orbitron text-white'>Data Innovations Conference Experts</span>
-        <Marquee className='bg-black mt-6'>
-            <ProfileCard name="Natalia"/>
-            <ProfileCard name="Vishal"/>
-            <ProfileCard name="Priyasnyi"/>
-            <ProfileCard name="vbkbkn"/>
-            <ProfileCard/>
-        </Marquee>        
-    </div>
-        
+      <div className="w-full min-h-[45rem] bg-black text-center flex flex-col gap-2 p-4 sm:p-6 md:p-7 pt-8 sm:pt-10 md:pt-11">
+        <span className="text-white text-2xl sm:text-3xl md:text-4xl font-orbitron">
+          Data Innovations Conference Experts
+        </span>
+        <span className="text-center font-orbitron text-white text-sm sm:text-base">
+          Data Innovations Conference Experts
+        </span>
+        <div className="mt-4 sm:mt-5 md:mt-6">
+          <Marquee className="bg-black">
+            <ProfileCard name="Natalia" />
+            <ProfileCard name="Vishal" />
+            <ProfileCard name="Priyasnyi" />
+            <ProfileCard name="vbkbkn" />
+            <ProfileCard />
+          </Marquee>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Speakers
+export default Speakers;
--- src/constants/ProfileCard.jsx
+++ src/constants/ProfileCard.jsx
@@ -6,18 +6,30 @@ import {
   Typography,
   Tooltip,
 } from "@material-tailwind/react";
- 
-export function ProfileCard({name}) {
+
+export function ProfileCard({ name }) {
   return (
-    <Card className="w-96 p-[2rem]  bg-black border-dashed border-[5px] border-[#72CBD3] ml-[2rem] mt-7">
+    <Card className="w-64 sm:w-72 md:w-80 lg:w-96 p-4 sm:p-6 md:p-[2rem] bg-black border-dashed border-2 sm:border-3 md:border-[5px] border-[#72CBD3] mx-auto sm:ml-[1rem] md:ml-[2rem] mt-4 sm:mt-5 md:mt-7">
       <CardHeader floated={false} className="bg-transparent">
-        <img src="https://docs.material-tailwind.com/img/team-3.jpg" alt="profile-picture" className="rounded-full"/>
+        <img
+          src="https://docs.material-tailwind.com/img/team-3.jpg"
+          alt="profile-picture"
+          className="rounded-full w-24 sm:w-28 md:w-32 mx-auto"
+        />
       </CardHeader>
       <CardBody className="text-center">
-        <Typography variant="h4" color="blue-gray" className="mb-2" >
+        <Typography
+          variant="h4"
+          color="blue-gray"
+          className="mb-2 text-lg sm:text-xl md:text-2xl"
+        >
           {name || "Souvik"}
         </Typography>
-        <Typography color="blue-gray" className="font-medium" textGradient>
+        <Typography
+          color="blue-gray"
+          className="font-medium text-sm sm:text-base"
+          textGradient
+        >
           CEO / Co-Founder
         </Typography>
       </CardBody>
