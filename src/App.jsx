b99be0ddcae5e7db8c99849b056745a76ffc2d20
Priyanshu Dash <pdash3774@gmail.com>
2024-08-08 22:28:00 +0530

    fixed all the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import Loadr from "./components/Loadr";
+import Loadr from "./components/Loadr";
 import Home from "./components/Home";
 import Event from './components/Event';
 
 function App() {
-  // const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  // useEffect(() => {
-  //   setLoading(true);
-  //   setTimeout(() => {
-  //     setLoading(false)
-  //   }, 4000)
-  // }, [])
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false)
+    }, 4000)
+  }, [])
 
-  // if (loading) {
-  //   return <Loadr />;
-  // }
+  if (loading) {
+    return <Loadr />;
+  }
 
   return (
     <Router>
