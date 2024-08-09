9b19a1a2ae28dd82968cc2dca2b4e107e50d6cd4
Priyanshu Dash <pdash3774@gmail.com>
2024-08-09 13:57:33 +0530

    fixed th ht

--- src/Events/EventDetails.js
+++ src/Events/EventDetails.js
@@ -10,14 +10,6 @@ const events = [
   },
   {
     id: 2,
-    title: "AI Workshop",
-    description: "Learn about artificial intelligence",
-    about: "Datathon is an intense 24-hour hackathon that serves as a cornerstone event of Datatrix'24, a prestigious symposium organized by the Department of Computer Science and Engineering. Focused on the theme of Data Science, Data Analytics, and related domains, this event challenges participants to leverage their expertise to develop innovative solutions to real-world problems centered around Big Data Analytics.Participants, whether seasoned coders or data enthusiasts, are invited to push their skills to the limit, collaborating with like-minded peers to create impactful, data-driven solutions. The event includes mentoring rounds with experts, providing guidance and insights that can elevate projects to the next level.Datathon is more than just a competition—it's a platform for creativity, collaboration, and the application of cutting-edge data science techniques. Whether you're looking to showcase your skills or learn from industry leaders, Datathon offers a unique opportunity to dive deep into the world of data and make a meaningful impact.",
-    image:
-      "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
-  },
-  {
-    id: 3,
     title: "Data Science Quiz",
     description: "24-hour coding challenge",
     about: "An exciting quiz competition to challenge participants' knowledge of big data concepts, tools, and technologies. This event tests expertise across a wide range of topics, from foundational principles to advanced analytics techniques, providing a thrilling opportunity to see how participants stack up against the best in the field.",
@@ -25,7 +17,7 @@ const events = [
       "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
   },
   {
-    id: 4,
+    id: 3,
     title: "Speed Regex",
     description: "24-hour coding challenge",
     about: "An event determined to test the accuracy and speed of the participants in constructing Regular Expressions, making it a fun and exciting challenge for all the Regex enthusiasts! participants may be tasked with matching a specific word in a string, validating dates in, or handling nested patterns. Test your quick thinking and problem solving skills under pressure.",
@@ -33,7 +25,7 @@ const events = [
       "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
   },
   {
-    id: 5,
+    id: 4,
     title: "Idea Explorer",
     description: "24-hour coding challenge",
     about: '“"Idea Explorer" is a lively forum where students from throughout the country come together to showcase their unique ideas and initiatives. This lively event aims to inspire, educate, and connect those who care about creativity and innovation. You will become part of a community that values creativity, teamwork, and an entrepreneurial spirit by choosing us. Whether you participate or attend, you will have the opportunity to learn from industry experts, network with other innovators, and watch the emergence of game-changing ideas that will influence the future.',
@@ -41,7 +33,7 @@ const events = [
       "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
   },
   {
-    id: 6,
+    id: 5,
     title: "Design Forge",
     description: "UI/UX Workshop & Competition",
     about: "Join us at DesignForge for a dynamic UI/UX experience, starting with a workshop led by industry experts where you'll sharpen your design skills and learn the latest trends. Then, put your knowledge to the test in our exciting competition, where designers create innovative user experiences. This is your chance to learn, compete, and showcase your talent in the fast-paced world of UI/UX design.",
@@ -49,13 +41,13 @@ const events = [
       "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
   },
   {
-    id: 7,
+    id: 6,
     title: "Data Visulalization Challenge",
     description: "24-hour coding challenge",
     about: "A place where students who are interested in data visualization can showcase their skills. A sample data set will be given, and the participants can make exciting interactive dashboards using any of their favorite frameworks, like Tableau, PowerBi, d3.js, etc. Judging would be based on students' creativity and ability to showcase a story via their dashboard.",
     image:
       "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
-  },
+  }
 ];
 
 export default events;
--- src/components/TechEvents.jsx
+++ src/components/TechEvents.jsx
@@ -5,7 +5,7 @@ import events from '../Events/EventDetails'
 const TechEvents = () => {
 
   return (
-    <div className="bg-event w-full p-4 sm:p-6 md:p-8 lg:p-[4rem] relative lg:pt-[10rem] bg-cover h-[83rem]">
+    <div className="bg-event w-full p-4 sm:p-6 md:p-8 lg:p-[4rem] relative lg:pt-[10rem] bg-cover lg:h-[82rem] md:h-[75rem] h-[200rem]">
       <div className="bg-transparent text-2xl sm:text-3xl md:text-4xl text-white font-orbitron text-center mb-6 sm:mb-8 md:mb-[3rem]">
         <span>Technical Events</span>
       </div>
