c81d6041bb0acc4e584d7ab75e94c1cb0d64908d
Eeshwar S <86971180+darkboi3301@users.noreply.github.com>
2024-08-22 00:39:24 +0530

    chore: Update Event component to handle payment response and insert registration details

--- src/components/updateworker.js
+++ src/components/updateworker.js
@@ -4,89 +4,177 @@ import renderHtml from "./renderHtml.js";
 var basic_default = {
     async fetch(request, env) {
 
+        const headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Content-Type': 'application/x-www-form-urlencoded',
+        };
+
         if (request.method === 'OPTIONS') {
-            return new Response(null, {
-                headers: {
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
-                    "Access-Control-Allow-Headers": "Content-Type",
-                    "Content-Type": "application/json"
-                }
-            });
+            return new Response(null, { headers });
         }
+
+        // content is coming in formdatan
 
         if (request.method === 'POST') {
-            const supabase = createClient(env.SUPABASE_URL, env.SUPABASE_KEY);
-
-            const body = await request.json();
-            // insert into transactions table
-            const { data, error } = await supabase.from('transactions').insert([{
-                txnid: body.txnid,
-                firstname: body.firstname,
-                email: body.email,
-                phone: body.phone,
-                key: body.key,
-                mode: body.mode,
-                status: body.status,
-                unmappedstatus: body.unmappedstatus,
-                cardCategory: body.cardCategory,
-                addedon: body.addedon,
-                payment_source: body.payment_source,
-                PG_TYPE: body.PG_TYPE,
-                bank_ref_num: body.bank_ref_num,
-                bankcode: body.bankcode,
-                error: body.error,
-                name_on_card: body.name_on_card,
-                cardnum: body.cardnum,
-                issuing_bank: body.issuing_bank,
-                card_type: body.card_type,
-                easepayid: body.easepayid,
-                amount: body.amount,
-                net_amount_debit: body.net_amount_debit,
-                cash_back_percentage: body.cash_back_percentage,
-                deduction_percentage: body.deduction_percentage,
-                productinfo: body.productinfo,
-                udf10: body.udf10,
-                udf9: body.udf9,
-                udf8: body.udf8,
-                udf7: body.udf7,
-                udf6: body.udf6,
-                udf5: body.udf5,
-                udf4: body.udf4,
-                udf3: body.udf3,
-                udf2: body.udf2,
-                udf1: body.udf1,
-                hash: body.hash,
-                surl: body.surl,
-                furl: body.furl,
-                error_Message: body.error_Message,
-                merchant_logo: body.merchant_logo,
-                upi_va: body.upi_va
-            }]);
-            if (error) {
-                console.log("error", error);
-            } else {
-                console.log(data);
-            }
-
-            //  update status in registration table
-            const { data: data1, error: error1 } = await supabase.from('registration').update({
-                payment_status: body.status
-            }).eq('email', body.email);
-            if (error1) {
-                console.log("error", error1);
-            } else {
-                console.log(data1);
-            }
-
-            return new Response(data ? JSON.stringify(data) : error, {
-                status: 200,
-                headers: {
-                    "Content-Type": "text/html"
-                }
+            // return back the form data
+            let data = await request.formData();
+            let obj = {};
+            data.forEach((value, key) => {
+                obj[key] = value
             });
+            console.log(obj);
+            // insert the data into the database
+            const supabase = createClient(env.SUPABASE_URL, env.SUPABASE_KEY);
+            // handle the data for null values
+            if (obj.txnid === null) {
+                obj.txnid = "";
+            }
+            if (obj.firstname === null) {
+                obj.firstname = "";
+            }
+            if (obj.email === null) {
+                obj.email = "";
+            }
+            if (obj.phone === null) {
+                obj.phone = "";
+            }
+            if (obj.key === null) {
+                obj.key = "";
+            }
+            if (obj.mode === null) {
+                obj.mode = "";
+            }
+            if (obj.status === null) {
+                obj.status = "";
+            }
+            if (obj.unmappedstatus === null) {
+                obj.unmappedstatus = "";
+            }
+            if (obj.cardCategory === null) {
+                obj.cardCategory = "";
+            }
+            if (obj.addedon === null) {
+                obj.addedon = "";
+            }
+            if (obj.payment_source === null) {
+                obj.payment_source = "";
+            }
+            if (obj.PG_TYPE === null) {
+                obj.PG_TYPE = "";
+            }
+            if (obj.bank_ref_num === null) {
+                obj.bank_ref_num = "";
+            }
+            if (obj.bankcode === null) {
+                obj.bankcode = "";
+            }
+            if (obj.error === null) {
+                obj.error = "";
+            }
+            if (obj.name_on_card === null) {
+                obj.name_on_card = "";
+            }
+            if (obj.cardnum === null) {
+                obj.cardnum = "";
+            }
+            if (obj.issuing_bank === null) {
+                obj.issuing_bank = "";
+            }
+            if (obj.card_type === null) {
+                obj.card_type = "";
+            }
+            if (obj.easepayid === null) {
+                obj.easepayid = "";
+            }
+            if (obj.amount === null) {
+                obj.amount = "";
+            }
+            if (obj.net_amount_debit === null) {
+                obj.net_amount_debit = "";
+            }
+            if (obj.cash_back_percentage === null) {
+                obj.cash_back_percentage = "";
+            }
+            if (obj.deduction_percentage === null) {
+                obj.deduction_percentage = "";
+            }
+            if (obj.productinfo === null) {
+                obj.productinfo = "";
+            }
+            if (obj.udf10 === null) {
+                obj.udf10 = ""; 
+            }
+            if (obj.udf9 === null) {
+                obj.udf9 = "";
+            }
+            if (obj.udf8 === null) {
+                obj.udf8 = "";
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+            const { data1, error } = await supabase.from('public.payments').insert([
+                {
+                    txtid: obj.txnid,
+                    firstname: obj.firstname,
+                    email: obj.email,
+                    phone: obj.phone,
+                    key: obj.key,
+                    mode: obj.mode,
+                    status: obj.status,
+                    unmappedstatus: obj.unmappedstatus,
+                    cardCategory: obj.cardCategory,
+                    addedon: obj.addedon,
+                    payment_source: obj.payment_source,
+                    PG_TYPE: obj.PG_TYPE,
+                    bank_ref_num: obj.bank_ref_num,
+                    bankcode: obj.bankcode,
+                    error: obj.error,
+                    name_on_card: obj.name_on_card,
+                    cardnum: obj.cardnum,
+                    issuing_bank: obj.issuing_bank,
+                    card_type: obj.card_type,
+                    easepayid: obj.easepayid,
+                    amount: obj.amount,
+                    net_amount_debit: obj.net_amount_debit,
+                    cash_back_percentage: obj.cash_back_percentage,
+                    deduction_percentage: obj.deduction_percentage,
+                    productinfo: obj.productinfo,
+                    udf10: obj.udf10,
+                    udf9: obj.udf9,
+                    udf8: obj.udf8,
+                    udf7: obj.udf7,
+                    udf6: obj.udf6,
+                    udf5: obj.udf5,
+                    udf4: obj.udf4,
+                    udf3: obj.udf3,
+                    udf2: obj.udf2,
+                    udf1: obj.udf1,
+                    hash: obj.hash,
+                    surl: obj.surl,
+                    furl: obj.furl,
+                    error_Message: obj.error_Message,
+                    merchant_logo: obj.merchant_logo,
+                    upi_va: obj.upi_va
+                }
+            ]);
+            if (error) {
+                return new Response(JSON.stringify(error), { status: 500 });
+            }
+            return new Response(JSON.stringify(data1), { status: 200 });
         }
-    }
+    },
 };
 export {
     basic_default as default
