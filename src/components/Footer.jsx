d73a1faa66e52bef2b106326bd57a8abcbb4ca6c
Varsha RAS <125282266+VarshaRAS@users.noreply.github.com>
2024-08-22 05:56:22 +0000

    footerFinal

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,19 +6,7 @@ export function Footer() {
     <footer className="w-full bg-black p-8">
       <div className="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-clack text-center md:justify-between">
         <img src="https://docs.material-tailwind.com/img/logo-ct-dark.png" alt="logo-ct" className="w-10 invert" />
-        <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
-          <li>
-            <Typography
-              as="a"
-              href="#"
-              color="blue-gray"
-              className="font-normal transition-colors hover:text-blue-500 text-white focus:text-blue-500"
-            >
-              <Link to="/gallery">Gallery</Link>
-              
-            </Typography>
-          </li>
-          
+        <ul className="flex flex-wrap items-center gap-y-2 gap-x-8"> 
           <li>
             <Typography
               as="a"
@@ -33,7 +21,7 @@ export function Footer() {
       </div>
       <hr className="my-8 border-blue-gray-50" />
       <Typography color="blue-gray" className="text-center font-normal text-white">
-        &copy; 2024 Copyright : SRM Institute Of Science And Technology , CSE-BD , Priyanshu Dash
+        &copy; 2024 Copyright : SRM Institute Of Science And Technology , CSE-BDA , Datatrix Team
       </Typography>
     </footer>
   );
