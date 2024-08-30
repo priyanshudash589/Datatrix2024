b002b902e5459f3a04581061f1d644c50a734142
Souvik Ruhidas <102573237+Souvik8426@users.noreply.github.com>
2024-08-30 19:12:05 +0530

    added new sponsor

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -7,29 +7,36 @@ import daphne from '../assets/daphnetech.jpg'
 import codentatives from '../assets/Codentatives.png'
 import black from '../assets/black.png'
 import Nivellia_Fashion from '../assets/Nivellia_Fashion.png'
+import Guvi from '../assets/g-logo-1.svg'
+
 
 
 export function Sponsors() {
   return (
     <div className='bg-patt-grid p-4 sm:p-8 lg:p-[2rem] w-full text-center lg:pt-[5rem]'>
-    <span className='font-orbitron text-white p-6 text-2xl sm:text-3xl lg:text-4xl'>Our Sponsors</span>
+      <span className='font-orbitron text-white p-6 text-2xl sm:text-3xl lg:text-4xl'>Our Sponsors</span>
 
-  
 
-    <div className='p-4 sm:p-6 lg:p-[3rem] min-h-[20rem]'>
-        
+
+      <div className='p-4 sm:p-6 lg:p-[3rem] min-h-[20rem]'>
+
         <div className='grid grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-4'>
-            <img src={aanya} className='h-24 sm:h-32 lg:h-[15rem] mx-auto' alt="" />
-            <img src={mj} className='h-24 sm:h-32 lg:h-[15rem] mx-auto' alt="" />
-            <img src={grid} className='h-24 sm:h-32 lg:h-[15rem] mx-auto' alt="" /> 
-            <img src={silly} className='h-24 sm:h-32 lg:h-[15rem] mx-auto' alt="" /> 
-            <img src={daphne} className='h-24 sm:h-32 lg:h-[15rem] mx-auto' alt="" /> 
-            <img src={codentatives} className='h-24 sm:h-32 lg:h-[15rem] mx-auto bg-white' alt="" /> 
-            <img src={black} className='h-24 sm:h-32 lg:h-[15rem] mx-auto' alt="" /> 
-            <img src={Nivellia_Fashion} className='h-24 sm:h-32 lg:h-[15rem] mx-auto' alt="" /> 
+          <img src={grid} className='h-24 sm:h-32 lg:h-[15rem] mx-auto' alt="" />
+          <img src={Guvi} className='h-24 sm:h-32 lg:h-[15rem] mx-auto bg-white' alt="" />
+          <img src={codentatives} className='h-24 sm:h-32 lg:h-[15rem] mx-auto bg-white' alt="" />
+          <img src={mj} className='h-24 sm:h-32 lg:h-[15rem] mx-auto' alt="" />
+          <img src={silly} className='h-24 sm:h-32 lg:h-[15rem] mx-auto' alt="" />
+          <img src={daphne} className='h-24 sm:h-32 lg:h-[15rem] mx-auto' alt="" />
+
+
         </div>
+        <div className='grid grid-cols-2 lg:grid-cols-2 gap-4 lg:gap-4 mt-4'>
+          <img src={Nivellia_Fashion} className='h-24 sm:h-32 lg:h-[15rem] mx-auto' alt="" />
+          {/* <img src={black} className="hidden lg:block h-24 sm:h-32 lg:h-[15rem] mx-auto" alt="" /> */}
+          <img src={aanya} className='h-24 sm:h-32 lg:h-[15rem] mx-auto' alt="" />
+        </div>
+      </div>
     </div>
-</div>
   )
 }
 
